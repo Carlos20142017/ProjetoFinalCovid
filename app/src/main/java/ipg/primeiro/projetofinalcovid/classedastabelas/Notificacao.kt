09112f986b93d46f15cbdc8eb220a6565ec1dd57
Carlos Borja <carlossocegadocruz@gmail.com>
2021-06-03 01:52:04 +0100

    Atualizado os dados nas tabelas notificacao, pessoa e teste

--- app/src/main/java/ipg/primeiro/projetofinalcovid/classedastabelas/Notificacao.kt
+++ app/src/main/java/ipg/primeiro/projetofinalcovid/classedastabelas/Notificacao.kt
@@ -4,13 +4,13 @@ import android.content.ContentValues
 import ipg.primeiro.projetofinalcovid.basedados.TabelaNotificacao
 import ipg.primeiro.projetofinalcovid.basedados.TabelaPessoas
 
-data class Notificacao (var alerta: String, var descricao: String, var resultado: String) {
+data class Notificacao (var alerta: String, var descricao: String, var resultado: String, var id_estrang_testes: Int) {
     fun toContentValues(): ContentValues {
         val valores= ContentValues()
         valores.put(TabelaNotificacao.CAMPO_ALERTA, alerta)
         valores.put(TabelaNotificacao.CAMPO_DESCRICAO, descricao)
         valores.put(TabelaNotificacao.CAMPO_RESULTADO, resultado)
-
+        valores.put(TabelaNotificacao.CAMPO_ID_ESTRANG_TESTES, id_estrang_testes)
         return valores
     }
 }
--- app/src/main/java/ipg/primeiro/projetofinalcovid/classedastabelas/Teste.kt
+++ app/src/main/java/ipg/primeiro/projetofinalcovid/classedastabelas/Teste.kt
@@ -1,16 +1,17 @@
 package ipg.primeiro.projetofinalcovid.classedastabelas
 
 import android.content.ContentValues
+import ipg.primeiro.projetofinalcovid.basedados.TabelaNotificacao
 import ipg.primeiro.projetofinalcovid.basedados.TabelaPessoas
 import ipg.primeiro.projetofinalcovid.basedados.TabelaTestes
 
-data class Teste(var temperatura: Float, var sintomas: String, var estado_saude: String) {
+data class Teste(var temperatura: Float, var sintomas: String, var estado_saude: String, var id_estrang_pessoas: Int) {
     fun toContentValues(): ContentValues {
         val valores= ContentValues()
         valores.put(TabelaTestes.CAMPO_TEMPERATURA, temperatura)
         valores.put(TabelaTestes.CAMPO_SINTOMAS, sintomas)
         valores.put(TabelaTestes.CAMPO_EST_SAUDE, estado_saude)
-
+        valores.put(TabelaTestes.CAMPO_ID_ESTRANG_PESSOAS, id_estrang_pessoas)
         return valores
     }
 }
