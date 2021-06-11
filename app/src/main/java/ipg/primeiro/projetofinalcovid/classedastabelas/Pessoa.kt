a6cf8f66fcf26af28393c409cb172033e2c9f4de
Carlos Borja <carlossocegadocruz@gmail.com>
2021-06-11 13:38:48 +0100

    Adicionar o nome do Distrito à classe Pessoa

--- app/src/main/java/ipg/primeiro/projetofinalcovid/classedastabelas/Pessoa.kt
+++ app/src/main/java/ipg/primeiro/projetofinalcovid/classedastabelas/Pessoa.kt
@@ -3,11 +3,13 @@ package ipg.primeiro.projetofinalcovid.classedastabelas
 import android.content.ContentValues
 import android.database.Cursor
 import android.provider.BaseColumns
+import ipg.primeiro.projetofinalcovid.basedados.TabelaDistritos
 import ipg.primeiro.projetofinalcovid.basedados.TabelaPessoas
 import java.util.*
 
 
-data class Pessoa (var id: Long = -1, var nome: String, var sexo: String, var data_nascimento: Date, var telemovel: String, var id_estrang_distrito: Long){
+data class Pessoa (var id: Long = -1, var nome: String, var sexo: String, var data_nascimento: Date,
+                   var telemovel: String, var id_estrang_distrito: Long, var nomeDistrito: String?){
      fun toContentValues(): ContentValues {
        val valores= ContentValues().apply {
            put(TabelaPessoas.CAMPO_NOME, nome)
@@ -28,6 +30,7 @@ data class Pessoa (var id: Long = -1, var nome: String, var sexo: String, var da
             val colDataNascimento = cursor.getColumnIndex(TabelaPessoas.CAMPO_DATA_NASCIMENTO)
             val colTelemovel = cursor.getColumnIndex(TabelaPessoas.CAMPO_TELEMOVEL)
             val colIdDistrito = cursor.getColumnIndex(TabelaPessoas.CAMPO_ID_ESTRANG_DISTRITO)
+            val colNomeDistrito = cursor.getColumnIndex(TabelaPessoas.CAMPO_EXTERNO_NOME_DISTRITO)
 
             val id = cursor.getLong(colId)
             val nome = cursor.getString(colNome)
@@ -35,8 +38,9 @@ data class Pessoa (var id: Long = -1, var nome: String, var sexo: String, var da
             val dataNascimento = cursor.getLong(colDataNascimento)
             val telemovel = cursor.getString(colTelemovel)
             val idDistrito = cursor.getLong(colIdDistrito)
+            val nomeDistrito = if(colNomeDistrito != -1) cursor.getString(colNomeDistrito) else null
 
-            return Pessoa (id, nome, sexo, Date(dataNascimento), telemovel, idDistrito)
+            return Pessoa (id, nome, sexo, Date(dataNascimento), telemovel, idDistrito, nomeDistrito)
         }
 
     }
