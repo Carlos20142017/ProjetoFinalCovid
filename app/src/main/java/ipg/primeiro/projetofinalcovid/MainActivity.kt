d0e69f85a872dbba1571031a9465d67f80817b2e
Carlos Borja <carlossocegadocruz@gmail.com>
2021-07-02 18:23:39 +0100

    Criado o Adapter de Distritos( e também ListaDistritoFragment)

--- app/src/main/java/ipg/primeiro/projetofinalcovid/MainActivity.kt
+++ app/src/main/java/ipg/primeiro/projetofinalcovid/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var menu: Menu
 
     var menuAtual = R.menu.menu_lista_pessoas
-    var menuAtualDistrito = R.menu.menu_lista_distrito
+
 
 
 
